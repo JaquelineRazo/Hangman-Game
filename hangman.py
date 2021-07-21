e84ef1f7e18528e4591e3997fbf93ff0fc1b5ecb
JaquelineRazo <jaquelinerazo.ordaz@gmail.com>
2021-07-21 17:23:53 -0500

    Add files via upload

---
+++ hangman.py
@@ -0,0 +1,55 @@
+import random
+from words import words
+import string
+
+def get_valid_word(words):
+    word =random.choice(words)  #randomly chooses something from the list
+    while '-' in word or ' ' in word:
+        word = random.choice(word)
+
+    return word.upper()
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in teh word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # letters what the user has guessed
+
+    lives = 7
+
+    #getting user input
+    while len(word_letters) > 0 and lives > 0:
+        # letters used
+        # ' '.join(['a','b','cd']) --> 'a b cd'
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
+
+        # what current word is (ie W - R D)
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ''.join(word_list))
+
+        user_letter = input('Guess letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
+            
+            else:
+                lives = lives - 1  # takes away a life if wrong
+                print('\nYour letter,', user_letter, 'is not in the word.')
+
+        elif user_letter in used_letters:
+            print('\nYou have already used that character. Please trye again.')
+
+        else:
+            print('\nInvalid character. Please try again.')
+
+    # gets here when len(word_letters) == 0 OR when lives == 0
+
+    if lives == 0:
+        print('You died, sorry. The word was', word)
+    else:
+        print('YAY! You guessed the word', word, '!!')
+
+if __name__ == '__main__':
+    hangman()
